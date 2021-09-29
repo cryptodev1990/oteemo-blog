e7de0df6a39cad037de5fcf28fbf06b8b67df324
cryptodev1990 <raylane19990105@gmail.com>
2021-09-29 09:05:18 -0700

    working CRUD backend with MongoDB, containerized versions will come soon

--- api/api.py
+++ api/api.py
@@ -1,10 +1,11 @@
 import time
 from flask import Flask, request, json, Response
 from pymongo import MongoClient
+import logging
 
 class MongoAPI:
     def __init__(self, data):
-        self.client = MongoClient("mongodb://localhost:27017/")
+        self.client = MongoClient("mongodb://localhost:5000/")
 
         database = data['database']
         collection = data['collection']
@@ -18,7 +19,7 @@ class MongoAPI:
         return output
     
     def write(self, data):
-        log.info('Writing Data')
+        logging.info('Writing Data')
         new_document = data['Document']
         response = self.collection.insert_one(new_document) 
         output = { 'Status': 'Successfully Inserted',
@@ -33,17 +34,71 @@ class MongoAPI:
         return output
 
     def delete(self, data):
-        filt = data['Document']
+        filt = data['Filter']
         response = self.collection.delete_one(filt)
         output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
         return output
 
 app = Flask(__name__)
 
-if __name__ == __main__:
-    data = {
-        "database": "BlogDB",
-        "collection": "posts"
-    }
-    mongo_obj = MongoAPI(data) 
-    print(json.dumps(mongo_obj.read(), indent=4))
+@app.route('/')
+def base():
+    return Response(response=json.dumps({"Status": "UP"}),
+            status=200,
+            mimetype='application/json')
+
+@app.route('/mongodb', methods=['GET'])
+def mongo_read():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                status=400,
+                mimetype='appplication/json')
+    obj1 = MongoAPI(data) 
+    response = obj1.read()
+    return Response(response=json.dumps(response),
+            status=200,
+            mimetype='application/json')
+
+@app.route('/mongodb', methods=['POST'])
+def mongo_write():
+    data = request.json
+    if data is None or data == {} or 'Document' not in data:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                status=400,
+                mimetype='application/json')
+    obj1 = MongoAPI(data) 
+    response = obj1.write(data) 
+    return Response(response=json.dumps(response),
+            status=200,
+            mimetype='application/json')
+
+
+@app.route('/mongodb', methods=['PUT'])
+def mongo_update():
+    data = request.json
+    if data is None or data == {} or 'DataToBeUpdated' not in data:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPI(data)
+    response = obj1.update()
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+@app.route('/mongodb', methods=['DELETE'])
+def mongo_delete():
+    data = request.json
+    if data is None or data == {} or 'Filter' not in data:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400,
+                        mimetype='application/json')
+    obj1 = MongoAPI(data)
+    response = obj1.delete(data)
+    return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001, host='0.0.0.0')
