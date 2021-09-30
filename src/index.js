7e2be2ac7c3cae3b8377d308eb2b2f0287d3f83a
cryptodev1990 <raylane19990105@gmail.com>
2021-09-30 15:21:09 -0400

    reverting back

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from './button';
+import {BrowserRouter} from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
+      <BrowserRouter>
     <App />
+        </BrowserRouter>,
+      <Button/>
   </React.StrictMode>,
   document.getElementById('root')
 );
